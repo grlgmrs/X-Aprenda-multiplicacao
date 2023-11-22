7a4fab457ae1b0479c8001b90f4718b0f68b6157
Laryssa Patez <contatolaryssapatez@gmail.com>
2023-11-22 20:32:26 -0300

    :construction: Make window resizable

--- Assets/Scripts/Home/Home.cs
+++ Assets/Scripts/Home/Home.cs
@@ -5,40 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class HomeMain : MonoBehaviour
 {
-    public void OnButtonPlayClick()
-    {
-        SceneManager.LoadScene("Scenes/InfiniteMode"); //(Scene que deverá ser aberta quando clicado)
-    }
-    public void OnButtonHintsClick()
-    {
-        SceneManager.LoadScene("Scenes/Hints", LoadSceneMode.Additive); //(Scene que deverá ser aberta quando clicado)
-    }
-    public void OnButtonMultiplicationClick()
-    {
-        OpenMultiplicationTable();
-    }
-    public void OnButtonCreditsClick()
-    {
-        SceneManager.LoadScene("Scenes/Credits"); //(Scene que deverá ser aberta quando clicado)
-    }
-    public void Quit()
-    {
-        Application.Quit(); //fecha jogo
-    }
+  public void OnButtonPlayClick()
+  {
+    SceneManager.LoadScene("Scenes/InfiniteMode"); //(Scene que deverá ser aberta quando clicado)
+  }
+  public void OnButtonHintsClick()
+  {
+    SceneManager.LoadScene("Scenes/Hints", LoadSceneMode.Additive); //(Scene que deverá ser aberta quando clicado)
+  }
+  public void OnButtonMultiplicationClick()
+  {
+    OpenMultiplicationTable();
+  }
+  public void OnButtonCreditsClick()
+  {
+    SceneManager.LoadScene("Scenes/Credits"); //(Scene que deverá ser aberta quando clicado)
+  }
+  public void Quit()
+  {
+    Application.Quit(); //fecha jogo
+  }
 
-    private void OpenMultiplicationTable()
-    {
-        Texture2D multiplicationTableImage = Resources.Load<Texture2D>("tabuada");
+  private void OpenMultiplicationTable()
+  {
+    Texture2D multiplicationTableImage = Resources.Load<Texture2D>("tabuada");
 
-        string tempPath = Path.Combine(Application.temporaryCachePath, "tempMultiplicationTable.png");
-        SaveImage(tempPath, multiplicationTableImage);
-        Process.Start(tempPath);
-    }
+    string tempPath = Path.Combine(Application.temporaryCachePath, "tempMultiplicationTable.png");
+    SaveImage(tempPath, multiplicationTableImage);
+    Process.Start(tempPath);
+  }
 
-    private void SaveImage(string filePath, Texture2D image)
-    {
-        byte[] imageBytes = image.EncodeToPNG();
-        File.WriteAllBytes(filePath, imageBytes);
-    }
-
+  private void SaveImage(string filePath, Texture2D image)
+  {
+    byte[] imageBytes = image.EncodeToPNG();
+    File.WriteAllBytes(filePath, imageBytes);
+  }
 }
